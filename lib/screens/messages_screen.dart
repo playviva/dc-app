1447ac0739730b277127b15383e13e93fc433eb9
playviva <playvivagames@gmail.com>
2025-07-27 15:52:10 -0700

    Create messages_screen.dart

---
+++ lib/screens/messages_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MessagesScreen extends StatelessWidget {
+  const MessagesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Messages'),
+      ),
+      body: const Center(
+        child: Text('Messages page content coming soon.'),
+      ),
+    );
+  }
+}
