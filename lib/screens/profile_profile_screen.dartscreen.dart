8724d9e92bdcc8b32100cf28f97a2751a3f7a37f
playviva <playvivagames@gmail.com>
2025-07-27 15:52:48 -0700

    Create profile_screen.dart

---
+++ lib/screens/profile_profile_screen.dartscreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+climport 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: const Center(
+        child: Text('Profile page content coming soon.'),
+      ),
+    );
+  }
+}
+ass ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: const Center(
+        child: Text('Profile page content coming soon.'),
+      ),
+    );
+  }
+}
